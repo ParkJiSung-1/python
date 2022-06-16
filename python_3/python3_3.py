9864afa007eb4497481da486bcda0ccc90905699
박지성 <jiseong14@gmail.com>
2022-06-16 14:50:54 +0000

    20220618 23:50

---
+++ python_3/python3_3.py
@@ -0,0 +1,15 @@
+#숫자 처리 함수
+
+print(abs(-5)) #5 / 절대값
+print(pow(4,2)) #4^2 = 4 * 4 = 16
+print(max(5, 12)) #12 / 큰값
+print(min(5, 12)) #5
+print(round(3.14)) #3
+print(round(4.99)) #5
+
+from math import *
+print(floor(4.99)) #내림. 4
+print(ceil(3.14)) #올림. 4
+print(sqrt(16)) #제곱근. 4
+
+39:01
